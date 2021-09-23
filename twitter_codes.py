3d48ccb1bbeeae4998291e84274eac6f64376d7d
Eliška B <71393504+flower-girl-eli@users.noreply.github.com>
2021-09-23 19:15:53 +0200

    Create twitter_codes.py

---
+++ twitter_codes.py
@@ -0,0 +1,14 @@
+#Twitter codes
+#Register as a Twitter dev and then register your app/bot 
+#Then you will be able to obtain your keys
+#Fill them in here
+
+my_api_key = #API key
+my_api_secret = #API key secret
+
+my_api_security = ##########
+
+my_access_token = #Access token
+my_access_token_sec = #Access token secret
+
+my_bearer_token = ########## 
